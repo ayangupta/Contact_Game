0ebd99e7b6400ca79bb97f76bf799109c469c78e
f2014063 <aamodkharb@gmail.com>
2017-04-25 23:50:48 +0530

    Final

---
+++ client.c
@@ -0,0 +1,291 @@
+/*
+ *  Author: Sandeeep Kumar Mittal
+ *  Special Thanks to Prabodh Agarwal
+ *  Description: Client side file to guess a number over TCP/IP sockets.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <time.h>
+#include <fcntl.h>
+ //#include <conio.h>
+
+int main()
+{
+    // Important declaration
+    int clientSocket;
+    char *buffer;
+    struct sockaddr_in serverAddr;
+    socklen_t addr_size;
+    int leaderflag=0;
+    int hinterflag=0;
+
+    // User Input
+    char *userInput;
+
+    userInput = (char *)malloc(sizeof(char)*2000);
+    buffer = (char *)malloc(sizeof(char)*2000);
+
+    /*  Create the network socket.   */
+    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+    /*  1) PR_INET Internet domain
+        2) SOCK_STREAM represents Stream socket
+        3) 0 represents Default protocol (TCP in this case)
+        */
+
+    // Configure settings of the server address
+    /* Address family = Internet */
+    serverAddr.sin_family = AF_INET;
+
+    /* Set port number */
+    serverAddr.sin_port = htons(9001);
+    /*  In networking we prefer to use big-endian binary
+        sequencing.  Since in our PC we use small endian binary
+        scheme, we use htons to convert little endian to big
+        endian.
+        */
+
+    /* Set IP address to localhost */
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    /* Set all bits of the padding field to 0 */
+    memset(serverAddr.sin_zero, '\0', sizeof
+            serverAddr.sin_zero);
+
+    /* Connect the socket to the server using the address*/
+    addr_size = sizeof serverAddr;
+    if ( connect( clientSocket, (struct sockaddr *) &serverAddr,
+                addr_size) < 0 )
+    {
+        printf("[ERROR] Unable to connect to server.\n");
+        close( clientSocket );
+        exit(0);
+    }
+
+    int maxPoints = 1000;
+    int counter = 0;
+    unsigned int inputLength = 0;
+
+    // User input to guess a number
+    //printf("Attempt [%d] : ", ( counter + 1 ) );
+    //printf("Enter the number between 1 to 100 to send: ");
+    //while(1)
+    {
+        if ( recv ( clientSocket, buffer, 30, 0) < 0 )
+        {
+            printf("[ERROR] Connection closed prematurely.\n");
+           // break;
+        }
+
+        else
+        {
+            printf("%s\n",buffer);
+        }
+        bzero(buffer, 31);
+        scanf("%[^\n\t]s", userInput);
+
+        inputLength = strlen(userInput);
+        if ( send ( clientSocket, userInput, inputLength, 0) != inputLength )
+            printf("[ERROR] Unable to send the data.\n");
+        /* Read the message from the server into the buffer */
+    }
+   while(1){  {
+        if ( recv ( clientSocket, buffer, 30, 0) < 0 )
+        {
+            printf("[ERROR] Connection closed prematurely.\n");
+           // break;
+        }
+
+        else
+        {
+            printf("%s\n",buffer);
+        }
+        bzero(buffer, 31);
+        scanf("%s", buffer);
+        //printf("%s",buffer);
+
+        inputLength = strlen(buffer);
+        if ( send ( clientSocket, buffer, inputLength, 0) != inputLength )
+            printf("[ERROR] Unable to send the data.\n");
+        /* Read the message from the server into the buffer */
+    }
+
+  while(2>1){
+    read(clientSocket,buffer,499);
+    if(strcmp(buffer,"NEW")==0){printf("Round 1 Ends\n");bzero(buffer,499);break;}
+    if(strcmp(buffer,"Enter the word")==0)
+    {
+        printf("%s\n",buffer);
+        leaderflag=1;
+        bzero(buffer, 31);
+        scanf("%s", buffer);
+        inputLength = strlen(buffer);
+        if ( send ( clientSocket, buffer, inputLength, 0) != inputLength )
+            printf("[ERROR] Unable to send the data.\n");
+        bzero(buffer,499);
+        read(clientSocket,buffer,499);
+
+
+    }
+
+        printf("%s\n",buffer);
+
+	 bzero(buffer,499);
+        read(clientSocket,buffer,499);
+        printf("%s\n",buffer);
+
+
+        //printf("ohd");
+
+        bzero(buffer,499);
+        //read(clientSocket,buffer,499);
+        //printf("%s\n",buffer);
+        //printf("here\n");
+        bzero(buffer,499);
+        if(leaderflag==0)
+        scanf("%s", buffer);
+        //bzero()
+        //
+        write(clientSocket,buffer,499);
+        //printf("here is %s\n",buffer );
+        bzero(buffer,499);
+        //printf("here\n");
+        read(clientSocket,buffer,499);
+        //printf(" print this %s\n",buffer );
+        if(strcmp(buffer,"Your hint is selected. Enter the word")==0)
+        {   printf("(%s)\n",buffer );
+            bzero(buffer,499);
+            hinterflag=1;
+            scanf("%s",buffer);
+            write(clientSocket,buffer,499);
+            bzero(buffer,499);
+            read(clientSocket,buffer,499);
+
+
+        }
+
+      //while(1)
+        //{
+           // read(clientSocket,buffer,499);
+                    printf("Hint is %s\n",buffer );
+      if(leaderflag==0)
+        {
+            if(hinterflag==0)
+
+                {
+                    printf("Guess the word else type PASS\n");
+                    bzero(buffer,499);
+                    scanf("%s",buffer);
+                    write(clientSocket,buffer,499);
+                    //printf("yeh bheja");
+                    bzero(buffer,499);
+                    read(clientSocket,buffer,499);
+                    printf("%s\n",buffer );
+                }
+                else
+                {
+                    bzero(buffer,499);
+                    read(clientSocket,buffer,499);
+                    printf("%s\n",buffer );
+                    hinterflag=0;
+                }
+
+        }
+        if(leaderflag==1)
+        {
+            printf("You have three guesses\n");
+            printf("You can type PASS if you dont want to guess\n");
+
+            //bzero(buffer,499);
+            //read(clientSocket,buffer,499);
+            //printf("first GUESSSSSSS %s\n",buffer );
+            int pohs=1;
+            int gu=3;
+            while(gu!=0){
+                printf("GUESS no %d\n",gu);
+                bzero(buffer,499);
+                scanf("%s",buffer);
+                printf("%s\n",buffer );
+                write(clientSocket,buffer,499);
+                if(strcmp(buffer,"PASS")==0){gu=1;}
+                bzero(buffer,499);
+                read(clientSocket,buffer,499);
+                printf("%s\n",buffer);
+
+                if(strcmp(buffer,"CONGRATS .YOU ARE CORRECT")==0){gu=1;}
+                gu--;
+            }
+
+           /* if(strcmp(buffer,"PASS")!=0)
+            {
+                printf("guess %d %s\n",pohs, buffer);
+                //bzero(buffer,499);
+                if(strcmp(buffer,"Wrong")==0){
+                    bzero(buffer,499);
+                    scanf("%s",buffer);pohs++;
+                    write(clientSocket,buffer,499);
+                    bzero(buffer,499);
+                    read(clientSocket,buffer,499);
+                    printf("guess is %d %s\n",pohs,buffer);
+                    if(strcmp(buffer,"Wrong")==0)
+                    {
+                        bzero(buffer,499);
+                        scanf("%s",buffer);pohs++;
+                        write(clientSocket,buffer,499);
+                        bzero(buffer,499);
+                        read(clientSocket,buffer,499);
+                        printf("guess is %d %s\n",pohs,buffer);
+                    }
+                    else{printf("%s\n", );}
+                }
+                else{
+
+                }
+
+
+            }*/
+
+        }
+
+}
+}
+
+
+
+    /*int availablePoints = maxPoints;
+    while ( buffer[0] == '2' )
+    {
+        ++counter;
+        availablePoints = maxPoints - ( counter * 100 );
+        if ( availablePoints > 0 )
+        {
+            // Send data to server
+            printf("Attempt [%d] : ", ( counter + 1 ) );
+            printf("Enter the number between 1 to 100 to send: ");
+            scanf("%s", userInput);
+
+            inputLength = strlen(userInput);
+            if ( send ( clientSocket, userInput,
+                        inputLength, 0) != inputLength )
+                printf("[ERROR] Unable to send the data.\n");
+            if ( recv ( clientSocket, buffer, 10, 0) < 0 )
+                printf("[ERROR] Connection closed prematurely.\n");
+        } else {
+            userInput = "DONE\0";
+            send ( clientSocket, userInput, 5, 0);
+            break;
+        }
+    }
+    availablePoints = maxPoints - (counter * 100 );
+    */
+    close( clientSocket );
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,401 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+#include <fcntl.h>
+
+struct info
+{
+    int socketvalue;
+    char name[20];
+    int points;
+    int corr;
+};
+
+struct val
+{
+    int score;
+};
+
+/*void handle(int sig){
+    printf(" ------- \t\t\t\t ----------------------------\n  %s  \t\t\t\t %s\t%s\t%s\t%s  \n    %d    \t\t\t\t   %d\t  %d\t  %d\t  %d   \n ------- \t\t\t\t ----------------------------\n",timer,buffo[0],buffo[1],buffo[2],buffo[3],tim,scores[0],scores[1],scores[2],scores[3]);
+    //printf("|\t\t\t\t\t\t\t\t\t| \n");
+    //printf("  %s  ",timer);printf("\t\t\t\t %s\t%s\t%s\t%s  \n",buffo[0],buffo[1],buffo[2],buffo[3]);
+    //printf("    %d    ",time);printf("\t\t\t\t   %d\t  %d\t  %d\t  %d   \n",scores[0],scores[1],scores[2],scores[3]);
+    //printf(" ------- \t\t\t\t ----------------------------\n");
+    //printf("|\t\t\t\t\t\t\t\t\t| \n");
+    //printf("|\t%s\t\t%s\t\t%s\t\t%s\t\t| \n",buffo[0],buffo[1],buffo[2],buffo[3]);
+    //printf("|\t%d\t\t%d\t\t%d\t\t%d\t\t| \n",scores[0],scores[1],scores[2],scores[3]);
+    //printf("|\t\t\t\t\t\t\t\t\t| \n");
+    //printf("\t\t\t%s\t \n",hint);
+    //printf("\t\t%s\n",notice);
+    //printf("|\t\t\t\t\t\t\t\t\t| \n");
+    //printf("|\t\t\t\t\t\t\t\t\t| \n");
+    //printf("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n");
+    printf("\033[6A");
+    //scanf("%d",&scores[2]);
+    tim++;
+    alarm(1);
+}*/
+
+
+//char timer[]={"timer"};
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main() {
+
+    time_t endwait;
+    time_t start;
+    time_t seconds;
+    
+    //signal(SIGALRM, handle);
+    //alarm(1);
+    int tim = 1;
+
+    struct val* sc;    
+    struct info idname[5];
+    int sockfd, newsocketfd[5], newsockfd, portno;
+    socklen_t clilen;
+    char buffer[500];
+    struct sockaddr_in serv_addr, cli_addr[5];
+    int n;
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+         error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = 9001;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr[0]);
+    fcntl(sockfd, F_SETFL, fcntl(sockfd,F_GETFL, 0) | O_NONBLOCK);
+
+    //Below code is modified to handle multiple clients using fork
+    //------------------------------------------------------------------
+    int i=0,qwe;
+    start = time(NULL);
+    seconds = 10;
+    endwait = start + seconds;
+    do{
+
+         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr[i], &clilen);
+
+         if (newsockfd != -1)
+            {     
+            //printf(\r) 
+            //fcntl(newsockfd, F_SETFL, fcntl(newsockfd,F_GETFL, 0) | O_NONBLOCK);   
+            newsocketfd[i++] = newsockfd;
+            //do whatever you want
+            
+          } 
+    }while((start = time(NULL)) < endwait);
+    int j;
+    sc = calloc(i,sizeof(struct val));
+    for(j=0;j<i;j++)
+    {
+        n = write(newsocketfd[j], "Enter you Name", 15);
+        if (n < 0)
+             error("ERROR writing to socket");
+         idname[j].corr=0;
+         idname[j].points=0;
+
+            //error("ERROR reading from socket");
+            
+    }
+    for(j=0;j<i;j++)
+    {
+        bzero(buffer, 256);
+        n = read(newsocketfd[j], buffer, 499);
+        idname[j].socketvalue = newsocketfd[j];
+        strcpy(idname[j].name,buffer);
+        if (n >= 0)
+            printf("The name of the player is: %s from %d\n", idname[j].name,idname[j].socketvalue);       
+    }
+
+    while(1)
+    {
+    for(j=0;j<i;j++)
+    {
+        n = write(newsocketfd[j], "Do you want to be a leader", 26);
+        if (n < 0)
+             error("ERROR writing to socket");
+            //error("ERROR reading from socket");
+    }
+    int oi=1;
+    int leader;
+    char leadername[20];
+    start = time(NULL);
+    seconds = 10;
+    endwait = start + seconds;
+    for(j=0;j<i;j++)
+    {
+        //do
+        {
+            bzero(buffer, 499);
+            n = read(newsocketfd[j], buffer, 499);
+            //if (n > 0)
+            {
+                printf("Answer: %s from %d\n", buffer,idname[j].socketvalue);
+                if((strcmp(buffer,"yes")==0)&& oi==1)
+                {
+                    write(newsocketfd[j],"Enter the word",255);oi=-91;
+                    leader=newsocketfd[j];
+                    printf("(leader =%d)\n",leader );
+                    strcpy(leadername,idname[j].name);
+                   }                                     
+                //break;
+            }
+        }// while ((start = time(NULL)) < endwait);
+    }
+    bzero(buffer, 499);
+    read(leader,buffer,499);
+    char word[35];
+    strcpy( word,buffer);
+    int length=strlen(word);
+    char hint[length];
+    hint[0]=word[0];
+    int fo=1;
+            int hindex=1;
+    for(fo=1;fo<length;fo++)
+    {
+        hint[fo]='_';
+    }
+    while(strcmp(hint,word)!=0)
+        {
+            bzero(buffer, 499);
+            strcpy(buffer,hint);
+            strcat(buffer,"(");
+            char stri[3];
+            sprintf(stri,"%d",length);
+            strcat(buffer,stri);
+            strcat(buffer,")");
+            strcat(buffer,"and the leader is ");
+            strcat(buffer,leadername);
+
+            
+            j=0;
+            for(j=0;j<i;j++)
+            {   printf(" %s \n",buffer);
+                write(newsocketfd[j],buffer,499);
+                if (n < 0)
+                     error("ERROR writing to socket");
+                    //error("ERROR reading from socket");
+            }
+
+    
+            j=0;
+            for(j=0;j<i;j++){
+                if(newsocketfd[j]==leader)write(newsocketfd[j],"Other people are typing hints now",499);
+               else{ write(newsocketfd[j],"You can now type the hints else type PASS",499);
+                if (n < 0)
+                     error("ERROR writing to socket");  
+            }}
+            j=0;
+            char hhint[200];
+            char hword[30];
+            int hinter;
+            int bk=0;
+            for(j=0;j<i;j++)
+            {
+                if(newsocketfd[j]==leader){printf("\n");}
+                else
+                    {
+                        bzero(buffer,499);
+                        read(newsocketfd[j], buffer, 499);
+                        
+                        
+
+                        //printf("%s\n",buffer );
+                        if(strcmp(buffer,"PASS")!=0&&bk!=1)
+                            {   
+                                strcpy(hhint,buffer);
+                                bk=1;
+                                write(newsocketfd[j],"Your hint is selected. Enter the word",499);
+                                bzero(buffer,499);
+                                hinter=newsocketfd[j];
+                                read(newsocketfd[j],buffer,499);
+                                strcpy(hword,buffer);
+                                
+                            }
+                    }
+
+                //printf("%s\n",buffer);
+
+            }
+            printf("HINT-%s\n",hhint);
+            //printf("%s\n",hword );
+            j=0;
+            bzero(buffer,499);
+            strcpy(buffer,hhint);
+            for(j=0;j<i;j++)
+            {
+                write(newsocketfd[j],buffer,499);
+            }
+            j=0;
+            int guesses=3;
+            int noofcontacts=0;
+            int lcorrect=0;
+
+            for(j=0;j<i;j++)
+            {   if(newsocketfd[j]==leader)
+                {   bzero(buffer,499);
+                        read(leader,buffer,499);
+                        printf(" NEW TRY%s\n",buffer );
+                        printf("Type guess number %d", guesses);
+                        
+                    while(guesses!=0)
+                    {   
+                        printf("Type guess number %d\n", guesses);
+                        bzero(buffer,499);
+                        read(leader,buffer,499);
+
+                        printf(" output of read %s\n",buffer );
+                        if(strcmp(buffer,"PASS")==0)
+                        {
+                            write(leader,"PASS",499);
+                            guesses=0;
+
+                        }
+                        else
+                        {   
+                            if(strcmp(buffer,hword)==0)
+                            {   write(leader,"CONGRATS .YOU ARE CORRECT",499);
+                                printf("Leader guessed correctly\n");      
+                                guesses=0;
+                                lcorrect=1;       
+                                sc[j].score++; 
+                                idname[j].points+=2;
+
+                            }
+                            else{
+                                write(leader,"Wrong",499);
+                                printf("Wrong, guesses left = %d   %s\n",(guesses-1),buffer);
+                                bzero(buffer,499);
+                                //read(leader,buffer,499);
+                                
+                                
+                                guesses--;
+                            }
+                            //bzero(buffer,499);
+                            //read(leader,buffer,499);
+
+                        }
+                    }
+                }
+                else
+                {
+                    if(newsocketfd[j]==hinter)
+                        {
+                            write(hinter,"Wait for people to contact",499);
+                        }
+                    else
+                    {   
+                        read(newsocketfd[j],buffer,499);
+                        printf("%s\n",buffer );
+
+                        //j=0;
+                        if(strcmp(buffer,hword)==0)
+                            {   write(newsocketfd[j],"CONGRATS .YOU ARE CORRECT",499);
+                                //printf("leader guessed correctly\n");      
+                                //guesses=0;            
+                                //idname[j].points+=5;      
+                                noofcontacts++;
+                                sc[j].score++;  sc[hinter].score++;
+                            }
+                        else
+                        {
+
+                            write(newsocketfd[j],"You are wrong",499);
+                        }
+                    }
+                } 
+                
+
+
+
+                //bzero(buffer,499);
+                //ead(newsocketfd[j],buffer,499);
+                //bzero(buffer,499);
+                
+            }
+
+            if(noofcontacts!=0&&lcorrect==0){
+                    hint[hindex]=word[hindex];
+                    printf("Modified hint %s\n",hint );
+                    hindex++;
+                 //noofcontacts=0;
+                }
+                        
+             j=0;
+                for(j=0;j<i;j++)
+
+                  { 
+                    if(lcorrect==0&&noofcontacts!=0){
+                        //hint[hindex]=word[hindex];
+                        //printf("modified hint %s\n",hint );
+                        //hindex++;
+                         
+                        if(newsocketfd[j]!=leader){
+                            //printf("points kyu nahi badh rahe\n");
+                            //printf("newsocketfd[j] - %d hinter %d\n",newsocketfd[j],hinter );
+                        if(newsocketfd[j]==hinter)
+                        {
+                            idname[j].points+=5;
+                            printf("points -  %d name - %s\n",idname[j].points,idname[j].name );
+                        }
+                        else{
+                            if(idname[j].corr=1)
+                                {
+                                idname[j].points+=5;
+                                idname[j].corr=0;
+                                printf("points -  %d name - %s\n",idname[j].points,idname[j].name );
+                                }
+                                else{
+                                    //printf("bakchodi MAX\n");
+                                }
+                           
+                            }     
+                        }
+                        //else{printf("leader hi hoon bhai\n");}
+
+                    }
+                        //else{printf("bas ye hi bacha tha\n");}
+                }
+                noofcontacts=0;
+               /*if(i==4)
+                sprintf(buffer," ------- \t\t\t\t ----------------------------\n  %s  \t\t\t\t\t %s\t%s\t%s  \n    %d    \t\t\t\t   %d\t  %d\t  %d\t     \n ------- \t\t\t\t ----------------------------\n",timer,idname[0].name,idname[1].name,idname[2].name,tim,sc[0].score,sc[1].score,sc[2].score);
+                else if(i==5)
+                sprintf(buffer," ------- \t\t\t\t ----------------------------\n  %s  \t\t\t\t %s\t%s\t%s\t%s  \n    %d    \t\t\t\t   %d\t  %d\t  %d\t  %d   \n ------- \t\t\t\t ----------------------------\n",timer,idname[0].name,idname[1].name,idname[2].name,idname[3].name,tim,sc[0].score,sc[1].score,sc[2].score,sc[3].score);
+                
+            for(j=0;j<i;j++)
+            {write(newsocketfd[j],buffer,499);
+            }*/
+            
+}
+ 
+  for(j=0;j<i;j++)
+    {
+                write(newsocketfd[j],"NEW",499);
+                
+                    
+               
+            
+    }   //fflush(stdout);
+    //printf("Out\rINn");
+    //fflush(stdout);
+}    //-------------------------------------------------------------------
+   return 0;
+}
